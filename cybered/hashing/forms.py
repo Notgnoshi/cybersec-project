1da35553081ba031fa01a076c1ae9a934726f2f7
ipiano <andrew@stelterfamily.net>
2019-03-09 17:48:40 -0700

    Made text inputs not look horrible

--- cybered/hashing/forms.py
+++ cybered/hashing/forms.py
@@ -4,11 +4,17 @@ from django import forms
 class TextBoxForm(forms.Form):
     """A simple textarea-only form."""
 
-    text = forms.CharField(widget=forms.Textarea(), label="")
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 3}), label=""
+    )
 
 
 class TextBoxWithPasswordForm(forms.Form):
     """A textarea form with an extra text box for a password."""
 
-    text = forms.CharField(widget=forms.Textarea(), label="")
-    secret_key = forms.CharField(label="Secret Key")
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 3}), label="Message"
+    )
+    secret_key = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"}), label="Secret Key"
+    )
