618fe683aa10283298c8e91920310340c7a68687
Austin Gill <notgnoshi@gmail.com>
2019-04-20 14:51:36 -0600

    Add first passwords lesson on verification and identification.
    The next page should walk through the process of hashing the passwords
    and comparing them to the list of hashes.

--- cybered/passwords/forms.py
+++ cybered/passwords/forms.py
@@ -7,3 +7,19 @@ class TextBoxForm(forms.Form):
     text = forms.CharField(
         widget=forms.Textarea(attrs={"class": "form-control", "rows": 3}), label=""
     )
+
+
+class AliceLoginForm(forms.Form):
+    """A sample login form with email and password."""
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "alice@wonderland.gov"}
+        ),
+        label="Email",
+        initial="alice@wonderland.gov",
+        disabled=True,
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"}), label="Password"
+    )
--- cybered/passwords/views/verification.py
+++ cybered/passwords/views/verification.py
@@ -2,13 +2,45 @@ from django.urls import reverse
 from django.views.generic import FormView, TemplateView
 
 from passwords.apps import PasswordsModule
-from passwords.forms import TextBoxForm
+from passwords.forms import TextBoxForm, AliceLoginForm
 
 from .mixin import PasswordsMixin
 
 
-class PasswordsVerificationView(PasswordsMixin, TemplateView):
-    pass
+class PasswordsVerificationView(PasswordsMixin, FormView):
+    form_class = AliceLoginForm
+    success_url = ""
+
+    def get_success_url(self):
+        return reverse(PasswordsModule.scope("verification"))
+
+    def form_valid(self, form):
+        self.request.session[PasswordsModule.scope("verification_email")] = form.cleaned_data[
+            "email"
+        ]
+        self.request.session[PasswordsModule.scope("verification_password")] = form.cleaned_data[
+            "password"
+        ]
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        # Only unlock the next page once they have entered the correct password.
+        key = PasswordsModule.scope("verification_password")
+        if self.request.session.get(key, "") == "IntoTheLookingGlass":
+            context = super().get_context_data(**kwargs)
+        else:
+            page_index = self.kwargs["page_index"]
+            context = super().get_context_data(disabled_pages=[page_index + 1], **kwargs)
+
+        key = PasswordsModule.scope("verification_password")
+        verification_password = self.request.session.get(key, "")
+        key = PasswordsModule.scope("verification_email")
+        verification_email = self.request.session.get(key, "")
+
+        context["verification_email"] = verification_email
+        context["verification_password"] = verification_password
+
+        return context
 
 
 class PasswordsVerificationToolView(PasswordsMixin, FormView):
