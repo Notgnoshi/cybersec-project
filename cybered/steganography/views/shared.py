4be329b9b59807d1de2118576d3350d7fd2cfa0a
ipiano <andrew@stelterfamily.net>
2019-04-28 17:38:42 -0600

    Added page progress lock

--- cybered/steganography/views/shared.py
+++ cybered/steganography/views/shared.py
@@ -4,8 +4,19 @@ from shared.src import cybered
 
 from django.views.generic import TemplateView
 
+
 class SteganographyMixin(SteganographyModule, cybered.PaginatedMixin, cybered.PageMixin):
-    pass
+    def get_context_data(self, disabled_pages=[], **kwargs):
+        page_index = self.kwargs["page_index"]
+        page_count = self.kwargs["page_count"]
+
+        key = SteganographyModule.scope("progress")
+        if key not in self.request.session:
+            self.request.session[key] = page_index
+        self.request.session[key] = max(self.request.session[key], page_index)
+
+        disabled_pages = disabled_pages + list(range(self.request.session[key] + 2, page_count))
+        return super().get_context_data(disabled_pages=disabled_pages, **kwargs)
 
 
 class ImageChoicesMixin:
