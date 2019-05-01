5db6fbe33ec66857c749429da36bd362594d572d
Austin Gill <notgnoshi@gmail.com>
2019-05-01 15:52:49 -0600

    Add trailing slashes to all URLs

--- cybered/hashing/src/hashing.py
+++ cybered/hashing/src/hashing.py
@@ -9,7 +9,7 @@ class HashingModule(cybered.LessonModule):
 
     module_name = "Hashing and Message Digests"
     module_base_link = "hashing/"
-    module_start_link = "begin"
+    module_start_link = "begin/"
 
     module_description = (
         "An introduction to message digests and their uses in message authentication and integrity"
@@ -19,11 +19,11 @@ class HashingModule(cybered.LessonModule):
 class HashingPageManager(cybered.PageManager):
     page_list = (
         ("begin", HashingModule.module_start_link, "hashing/begin.html"),
-        ("motivation", "motivation", "hashing/motivation.html"),
-        ("examples_form", "examples-input", "hashing/examples_form.html"),
-        ("examples_results", "examples-results", "hashing/examples_results.html"),
-        ("keyed_examples_form", "keyed-examples-input", "hashing/keyed_hashes_form.html"),
+        ("motivation", "motivation/", "hashing/motivation.html"),
+        ("examples_form", "examples-input/", "hashing/examples_form.html"),
+        ("examples_results", "examples-results/", "hashing/examples_results.html"),
+        ("keyed_examples_form", "keyed-examples-input/", "hashing/keyed_hashes_form.html"),
         ("keyed_examples_results", "keyed-examples-results", "hashing/keyed_hashes_results.html"),
-        ("conclusions", "conclusions", "hashing/conclusion.html"),
-        ("tools", "tools", "hashing/tools.html"),
+        ("conclusions", "conclusions/", "hashing/conclusion.html"),
+        ("tools", "tools/", "hashing/tools.html"),
     )
--- cybered/steganography/src/steganography.py
+++ cybered/steganography/src/steganography.py
@@ -1,24 +1,6 @@
 from shared.src import cybered
 
 
-class SteganographyPageManager(cybered.PageManager):
-    page_list = (
-        ("begin", "begin", "steganography/begin.html"),
-        ("image_metadata", "image-metadata", "steganography/image_metadata.html"),
-        (
-            "image_metadata_result",
-            "image-metadata-example-result",
-            "steganography/image_metadata_result.html",
-        ),
-        ("image_deltas1", "image-deltas1", "steganography/image_deltas1.html"),
-        ("image_deltas_result1", "image-deltas-result1", "steganography/image_deltas_result1.html"),
-        ("image_deltas2", "image-deltas2", "steganography/image_deltas2.html"),
-        ("image_deltas_result2", "image-deltas-result2", "steganography/image_deltas_result2.html"),
-        ("end", "conclusion", "steganography/conclusion.html"),
-        ("tools", "image-steganography-tools", "steganography/tools.html"),
-    )
-
-
 class SteganographyModule(cybered.LessonModule):
     """The cybered module configuration for the steganography module."""
 
@@ -27,5 +9,23 @@ class SteganographyModule(cybered.LessonModule):
 
     module_name = "Intro to Steganography"
     module_base_link = "steganography/"
-    module_start_link = "begin"
+    module_start_link = "begin/"
     module_description = "An introduction to basic steganographic techniques"
+
+
+class SteganographyPageManager(cybered.PageManager):
+    page_list = (
+        ("begin", SteganographyModule.module_start_link, "steganography/begin.html"),
+        ("image_metadata", "image-metadata/", "steganography/image_metadata.html"),
+        (
+            "image_metadata_result",
+            "image-metadata-example-result/",
+            "steganography/image_metadata_result.html",
+        ),
+        ("image_deltas1", "image-deltas1/", "steganography/image_deltas1.html"),
+        ("image_deltas_result1", "image-deltas-result1/", "steganography/image_deltas_result1.html"),
+        ("image_deltas2", "image-deltas2/", "steganography/image_deltas2.html"),
+        ("image_deltas_result2", "image-deltas-result2/", "steganography/image_deltas_result2.html"),
+        ("end", "conclusion/", "steganography/conclusion.html"),
+        ("tools", "image-steganography-tools/", "steganography/tools.html"),
+    )
